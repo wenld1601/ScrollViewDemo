e0247b95a49bbd620db3d71012ea07d855f768eb
Higgins Weng <wenld1601@gmail.com>
2019-10-19 19:01:58 -0700

    ScrollViewDemo

--- ScrollViewDemo/ViewController.swift
+++ ScrollViewDemo/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ScrollView: UIScrollView!
+    let imagesArray = ["image1", "image2", "image3"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in 0..<imagesArray.count{
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleToFill
+            imageView.image = UIImage(named: imagesArray[i])
+            let xPos = CGFloat(i)*self.view.bounds.size.width
+            imageView.frame = CGRect(x: xPos, y: 0, width: view.frame.size.width, height: ScrollView.frame.size.height)
+            ScrollView.contentSize.width = view.frame.size.width*CGFloat(i+1)
+            ScrollView.addSubview(imageView)
+        }
     }
-
 
 }
 
